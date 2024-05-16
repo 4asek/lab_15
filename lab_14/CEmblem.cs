7277bd2d4d1552d258342be548d105fd1d04c826
4asek <telefonnomiil4510@gmail.com>
2024-05-16 07:48:33 +0300

    Добавьте файлы проекта.

---
+++ lab_14/CEmblem.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_15
+{
+    class CEmblem : CFigure
+    {
+        const int DefaultRadius = 50;
+
+        private int _side;
+        private int _radius;
+        public int Side
+        {
+            get { return _side; }
+            set { _side = value >= 200 ? 200 : (value <= 5 ? 5 : value); }
+        }
+        public int Radius
+        {
+            get { return _radius; }
+            set { _radius = value >= 200 ? 200 : (value <= 5 ? 5 : value); }
+        }
+        public CEmblem(Graphics graphics, int X, int Y, int Side, int Radius)
+        {
+            this.graphics = graphics;
+            this.X = X; this.Y = Y;
+            this.Side = Side;
+            this.Radius = DefaultRadius;
+            this.Radius = Radius;
+        }
+        public CEmblem(Graphics graphics, int X, int Y, int Radius)
+        {
+            this.graphics = graphics; 
+            this.X = X; 
+            this.Y = Y;
+            this.Radius = Radius;
+        }
+        protected override void Draw(Pen pen)
+        {
+            // Малюємо коло
+            Rectangle circle = new Rectangle(X - Radius, Y - Radius, 2 * Radius, 2 * Radius);
+            graphics.DrawEllipse(pen, circle);
+
+            // Малюємо квадрат усередині кола (повернутий на 90 градусів)
+            Rectangle square = new Rectangle(X - Radius / 2, Y - Radius / 2, Radius, Radius);
+            graphics.RotateTransform(90); // Поворот на 90 градусів
+            graphics.DrawRectangle(pen, square);
+            graphics.ResetTransform(); // Скидаємо поворот
+
+            // Малюємо трикутник до гори низом
+            double r = (Side / 2) / Math.Sin(Math.PI / 3);
+            Point p1 = new Point(X, Y - (int)r);
+            Point p2 = new Point(X - (int)(r * Math.Cos(Math.PI / 6)), Y + (int)(r * Math.Sin(Math.PI / 6)));
+            Point p3 = new Point(X + (int)(r * Math.Cos(Math.PI / 6)), Y + (int)(r * Math.Sin(Math.PI / 6)));
+            Point[] triangle = { p1, p2, p3 };
+            graphics.DrawPolygon(pen, triangle);
+        }
+        override public void Expand(int dX)
+        {
+            Hide();
+            Side = Math.Min(200, Side + dX + 5);
+            Radius = Math.Min(200, Radius + dX);
+            Show();
+        }
+        override public void Collapse(int dX)
+        {
+            Hide();
+            Side = Math.Max(5, Side - dX - 5);
+            Radius = Math.Max(5, Radius - dX);
+            Show();
+        }
+       
+
+    }
+}
---
+++ lab_14/fMain.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_15
+{
+    public partial class fMain : Form
+    {
+        CFigure[] figures;
+        int FiguresCount = 0;
+        int CurrentFigureIndex;
+        public fMain()
+        {
+            InitializeComponent();
+            figures = new CFigure[100];
+            cbFigureType.SelectedIndex = 0;
+        }
+        
+        private void btnCreateNew_Click(object sender, EventArgs e)
+        {
+            if (FiguresCount >= 99)
+            {
+                MessageBox.Show("Досягнуто межі кількості об'єктів!"); return;
+            }
+            Graphics graphics = pnMain.CreateGraphics();
+            CurrentFigureIndex = FiguresCount;     // Створення нового об'єкта - фігури 
+            if (cbFigureType.SelectedIndex == 0)      // Створюємо коло 
+            {
+                figures[CurrentFigureIndex] =
+                    new CCircle(graphics, pnMain.Width / 2, pnMain.Height / 2,
+                        50);
+                cbFigures.Items.Add("Фігура №" + (FiguresCount - 1).ToString() +
+                    " [коло]");
+            }
+            else if (cbFigureType.SelectedIndex == 1) // Створюємо прямокутник 
+            {
+                figures[CurrentFigureIndex] = new CRectangle(graphics, pnMain.Width / 2, pnMain.Height / 2, 100, 50);
+                cbFigures.Items.Add("Фігура №" + (FiguresCount - 1).ToString() +
+                    " [прямокутник]");
+            }
+            else if (cbFigureType.SelectedIndex == 2) // Створюємо трикутник 
+            {
+                figures[CurrentFigureIndex] = new CTriangle(graphics, pnMain.Width / 2, pnMain.Height / 2, 100);
+                cbFigures.Items.Add("Фігура №" + (FiguresCount - 1).ToString() +
+                    " [трикутник]");
+            }
+            else if (cbFigureType.SelectedIndex == 3) // Створюємо емблему 
+            {
+                figures[CurrentFigureIndex] = new CEmblem(graphics, pnMain.Width / 2, pnMain.Height / 2, 88, 50);
+                cbFigures.Items.Add("Фігура №" + (FiguresCount - 1).ToString() +
+                    " [емблема]");
+            }
+            figures[CurrentFigureIndex].Show(); FiguresCount++;
+            cbFigures.SelectedIndex = FiguresCount - 1;
+        }
+
+        private void btnHide_Click(object sender, EventArgs e) 
+        {
+           CurrentFigureIndex = cbFigures.SelectedIndex;    
+            if ((CurrentFigureIndex > FiguresCount) ||  
+       (CurrentFigureIndex< 0))          return; 
+    // -------------------------------------------------------------- 
+    // Приховування поточного об'єкта
+    figures[CurrentFigureIndex].Hide(); 
+
+        }
+
+    private void btnShow_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+     (CurrentFigureIndex < 0)) return;
+
+            // -------------------------------------------------------------- 
+            // Відображення поточного об'єкта
+            figures[CurrentFigureIndex].Show(); 
+
+        }
+
+        private void btnExpand_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+   (CurrentFigureIndex < 0)) return;
+            // ------------------------------------------------------------- 
+            // Розширення поточного об'єкта
+            figures[CurrentFigureIndex].Expand(5); 
+
+        }
+
+        private void btnCollapse_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+    (CurrentFigureIndex < 0)) return;
+            // -------------------------------------------------------------     // Розширення поточного об'єкта - екземпляра класу CCircle
+            figures[CurrentFigureIndex].Collapse(5);     
+
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+(CurrentFigureIndex < 0)) return;
+            // -------------------------------------------------------------- 
+            // Переміщення вверх поточного об'єкта
+            figures[CurrentFigureIndex].Move(0, -10); 
+
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+   (CurrentFigureIndex < 0)) return;
+            // -------------------------------------------------------------- 
+            // Переміщення вниз поточного об'єкта
+            figures[CurrentFigureIndex].Move(0, 10);     
+
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+    (CurrentFigureIndex < 0)) return;
+            // -----------------------------------------
+            
+            // Переміщення вправо поточного об'єкта
+            figures[CurrentFigureIndex].Move(10, 0); 
+
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+     (CurrentFigureIndex < 0)) return;
+            // -------------------------------------------------------------- 
+            // Переміщення вліво поточного об'єкта
+            figures[CurrentFigureIndex].Move(-10, 0); 
+
+        }
+
+        private void btnRightFar_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+   (CurrentFigureIndex < 0)) return;
+            // --------------------------------------------------------------     // Переміщення поточного об'єкта вправо на більшу відстань
+            for (int i = 0; i < 100; i++) 
+            {
+                figures[CurrentFigureIndex].Move(1, 0);
+                System.Threading.Thread.Sleep(5);
+            }
+
+        }
+
+        private void btnLeftFar_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+   (CurrentFigureIndex < 0)) return;
+            // -------------------------------------------------------------- 
+            // Переміщення поточного об'єкта вліво на більшу відстань
+            for (int i = 0; i < 100; i++) 
+            {
+                figures[CurrentFigureIndex].Move(-1, 0);
+                System.Threading.Thread.Sleep(5);
+            }
+
+        }
+
+        private void btnUpFar_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+    (CurrentFigureIndex < 0)) return;
+            // -------------------------------------------------------------- 
+            // Переміщення поточного об'єкта вверх на більшу відстань
+            for (int i = 0; i < 100; i++) 
+            {
+                figures[CurrentFigureIndex].Move(0, -1); System.Threading.Thread.Sleep(5);
+            }
+
+        }
+
+        private void btnDownFar_Click(object sender, EventArgs e)
+        {
+            CurrentFigureIndex = cbFigures.SelectedIndex; if ((CurrentFigureIndex > FiguresCount) ||
+   (CurrentFigureIndex < 0)) return;
+            // -------------------------------------------------------------- 
+            // Переміщення поточного об'єкта вниз на більшу відстань
+            for (int i = 0; i < 100; i++) 
+            {
+                figures[CurrentFigureIndex].Move(0, 1);
+                System.Threading.Thread.Sleep(5);
+            }
+
+        }
+        private void fMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
